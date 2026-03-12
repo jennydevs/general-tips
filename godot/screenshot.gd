e89f0b6f77958347d94dbd70db6ae1e3b12b3f2a
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2026-03-12 15:27:57 -0400

    Update screenshot.gd

--- godot/screenshot.gd
+++ godot/screenshot.gd
@@ -1,4 +1,9 @@
 # get a quick screenshot
 
 var download_path = OS.get_system_dir(OS.SystemDir.SYSTEM_DIR_DOWNLOADS);
-var img = get_viewport().get_texture().get_image().save_png(download_path + "/name_here.png");
+var save_to_path = "/screenshot_name_here.png";
+
+# wait for current frame
+await RenderingServer.frame_post_draw;
+
+var img = get_viewport().get_texture().get_image().save_png(download_path + save_to_path);
